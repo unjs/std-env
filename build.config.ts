a64d40955046efbb5a39f92b64c43d6417812a33
Pooya Parsa <pyapar@gmail.com>
2022-04-15 22:42:32 +0200

    fix(pkg): emit cjs dist

--- build.config.ts
+++ build.config.ts
@@ -2,7 +2,9 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
-  emitCJS: true,
+  rollup: {
+    emitCJS: true
+  },
   entries: [
     'src/index'
   ]
