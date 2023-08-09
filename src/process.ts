1ad71f6e558241651249d442ea9b2e829f026a64
Pooya Parsa <pooya@pi0.io>
2023-08-09 11:53:05 +0200

    feat(process): support `process.versions`

--- src/process.ts
+++ src/process.ts
@@ -1,11 +1,28 @@
 import { env } from "./env";
 
-type StdProcess = Partial<typeof globalThis.process> & { env: typeof env };
+export interface Process
+  extends Partial<Omit<typeof globalThis.process, "versions">> {
+  env: Record<string, string | undefined>;
+  versions: Record<string, string>;
+}
 
-export const process = new Proxy<StdProcess>(globalThis.process || { env }, {
+const _process = (globalThis.process ||
+  Object.create(null)) as unknown as Process;
+
+const processShims = {
+  versions: {},
+};
+
+export const process = new Proxy<Process>(_process, {
   get(target, prop) {
+    if (prop === "env") {
+      return env;
+    }
     if (prop in target) {
       return target[prop];
+    }
+    if (prop in processShims) {
+      return processShims[prop as keyof typeof processShims];
     }
   },
 });
---
+++ test/deno.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+import * as stdEnv from "../dist/index.mjs";
+
+console.log(stdEnv.process.env.HOME);
