f97a04b7fba05970dd02601ac9d75bdb03ff9a77
Pooya Parsa <pooya@pi0.io>
2024-09-24 21:00:33 +0200

    chore: update repo

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import unjs from "eslint-config-unjs";
+
+// https://github.com/unjs/eslint-config
+export default unjs({
+  ignores: [],
+  rules: {
+  "unicorn/no-null": 0,
+  "unicorn/prevent-abbreviations": 0
+},
+});
