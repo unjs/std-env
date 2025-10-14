7de863c56bbf0829c09af633247ebbaa5b96ec94
Álvaro <alvaro@nxtfwd.com>
2025-10-14 11:23:00 +0200

    feat: detect google cloud run / job  (#161)

--- src/providers.ts
+++ src/providers.ts
@@ -19,6 +19,8 @@ export type ProviderName =
   | "cirrus"
   | "cloudflare_pages"
   | "cloudflare_workers"
+  | "google_cloudrun"
+  | "google_cloudrun_job"
   | "codebuild"
   | "codefresh"
   | "drone"
@@ -75,6 +77,8 @@ const providers: InternalProvider[] = [
   ["CIRRUS", "CIRRUS_CI"],
   ["CLOUDFLARE_PAGES", "CF_PAGES", { ci: true }],
   ["CLOUDFLARE_WORKERS", "WORKERS_CI", { ci: true }],
+  ["GOOGLE_CLOUDRUN", "K_SERVICE"],
+  ["GOOGLE_CLOUDRUN_JOB", "CLOUD_RUN_JOB"],
   ["CODEBUILD", "CODEBUILD_BUILD_ARN"],
   ["CODEFRESH", "CF_BUILD_ID"],
   ["DRONE"],
